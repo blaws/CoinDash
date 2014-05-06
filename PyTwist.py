530118939c340b01c1e9a829232f9215b0c6e343
amarti36 <amarti36@nd.edu>
2014-05-06 16:31:50 -0400

    added blit functions

--- PyTwist.py
+++ PyTwist.py
@@ -50,7 +50,7 @@ class PyTwist:
         self.spikegroundimage = pygame.image.load('images/Ground2.png').convert()
         self.groundrects = list()
         for i in range(0,721,120):
-            self.grounds.append(Ground(self, i, 360))
+            self.grounds.append(Ground(i, 360))
 #            self.groundrects.append(Rect(i, 360, 120, 120))
         self.coins = list()
 
@@ -106,7 +106,7 @@ class PyTwist:
                 for rect in self.groundrects:
                     print rect.x, rect.y
                     rect.x -= 5
-                    self.grounds.append(Ground(self, rect.x, rect.y))
+                    self.grounds.append(Ground(rect.x, rect.y))
 	for ground in self.grounds:
 		ground.tick()
 		if ground.rect.right < -120:
@@ -126,10 +126,10 @@ class PyTwist:
 	if self.count == 24:
 		self.count = 1
 		if (self.side == 0 and 1 != randint(0,9) and self.gap == 0) or self.connection == None:
-			self.grounds.append(Ground(self, 640, 360))
+			self.grounds.append(Ground(640, 360))
                         self.groundrects.append(self.grounds[-1].rect)
 		elif (self.side == 0 and self.gap == 4) or (self.side == 1 and self.addground == 2):
-			self.grounds.append(Ground(self, 640, 360))
+			self.grounds.append(Ground(640, 360))
                         self.groundrects.append(self.grounds[-1].rect)
 			self.gap = 0
 		else:
@@ -148,37 +148,14 @@ class PyTwist:
             self.connection.sendUpdate()
 
         # display
-        self.screen.blit(self.bg, self.bg_rect)
-        if self.bg_rect.right < self.width:
-            self.screen.blit(self.reverse_bg, self.bg_rect.move(self.width, 0))
-        else:
-            self.screen.blit(self.reverse_bg, self.bg_rect.move(-self.width, 0))
+	self.blit_bg()
         self.screen.blit(self.guardian.image, self.guardian.rect)
-	for platform in self.platforms:
-		self.screen.blit(platform.image, platform.rect)
-	for ground in self.grounds:
-            if self.grounds.index(ground) == 0 or abs(self.grounds[self.grounds.index(ground)-1].rect.x - ground.rect.x) > 120:
-		self.screen.blit(self.spikegroundimage, ground.rect)  #ground.image, 
-            else:
-		self.screen.blit(self.groundimage, ground.rect)  #ground.image, ground.rect)
-        for coin in self.coins:
-            if coin.rect.right > self.width:
-                tmp_rect = Rect(coin.rect)
-                tmp_rect.right = self.width
-                self.screen.blit(coin.image, tmp_rect)
-            else:
-                self.screen.blit(coin.image, coin.rect)
+	self.blit_platforms()
+	self.blit_grounds()
+	self.blit_coins()
         self.screen.blit(self.runner.image, self.runner.rect)
-	for wiley in self.wileys:
-		self.screen.blit(wiley.image, wiley.rect)
-	text = self.font.render(str(self.score), 1, (10, 10, 10))
-	textpos = text.get_rect()
-	self.screen.blit(text, textpos)
-	if self.connection == None:
-		connectionText = self.connectionFont.render("WAITING FOR PLAYER 2", 1, (255, 0, 0))
-		connectionTextpos = connectionText.get_rect()
-		connectionTextpos.y = 140
-		self.screen.blit(connectionText, connectionTextpos)
+	self.blit_wileys()
+	self.blit_text()
         pygame.display.flip()
 
     def move_background(self):
@@ -202,21 +179,13 @@ class PyTwist:
 
 	while self.explosion.animation != 15:
 		self.explosion.tick()
-		self.screen.blit(self.bg, self.bg_rect)
-		if self.bg_rect.right < self.width:
-		    self.screen.blit(self.reverse_bg, self.bg_rect.move(self.width, 0))
-		else:
-		    self.screen.blit(self.reverse_bg, self.bg_rect.move(-self.width, 0))
+		
+
+		self.blit_bg()
 		self.screen.blit(self.guardian.image, self.guardian.rect)
-		for platform in self.platforms:
-			self.screen.blit(platform.image, platform.rect)
-		for ground in self.grounds:
-                    if len(self.grounds) and self.grounds.index(ground) > 0 and abs(self.grounds[self.grounds.index(ground)-1].rect.x - ground.rect.x) > 120:
-                        self.screen.blit(self.spikegroundimage, ground.rect)  #ground.image, 
-                    else:
-                        self.screen.blit(self.groundimage, ground.rect)  #ground.image, ground.rect)
-		for wiley in self.wileys:
-			self.screen.blit(wiley.image, wiley.rect)
+		self.blit_platforms()
+		self.blit_grounds()
+		self.blit_wileys()
 		self.screen.blit(text, textpos)
 		self.screen.blit(scoreText, scoreTextpos)
 		self.screen.blit(self.explosion.image, self.explosion.rect)
@@ -224,6 +193,46 @@ class PyTwist:
 		pygame.display.flip()
 	reactor.stop()
 
+    def blit_coins(self):
+	for coin in self.coins:
+	    if coin.rect.right > self.width:
+		tmp_rect = Rect(coin.rect)
+		tmp_rect.right = self.width
+		self.screen.blit(coin.image, tmp_rect)
+	    else:
+		self.screen.blit(coin.image, coin.rect)
+
+    def blit_bg(self):
+	self.screen.blit(self.bg, self.bg_rect)
+	if self.bg_rect.right < self.width:
+	    self.screen.blit(self.reverse_bg, self.bg_rect.move(self.width, 0))
+	else:
+	    self.screen.blit(self.reverse_bg, self.bg_rect.move(-self.width, 0))
+	
+    def blit_platforms(self):
+	for platform in self.platforms:
+		self.screen.blit(platform.image, platform.rect)
+
+    def blit_grounds(self):
+	for ground in self.grounds:
+	    if self.grounds.index(ground) == 0 or abs(self.grounds[self.grounds.index(ground)-1].rect.x - ground.rect.x) > 120:
+		self.screen.blit(self.spikegroundimage, ground.rect)
+	    else:
+		self.screen.blit(self.groundimage, ground.rect)
+
+    def blit_wileys(self):
+	for wiley in self.wileys:
+		self.screen.blit(wiley.image, wiley.rect)
+
+    def blit_text(self):
+	text = self.font.render(str(self.score), 1, (10, 10, 10))
+	textpos = text.get_rect()
+	self.screen.blit(text, textpos)
+	if self.connection == None:
+		connectionText = self.connectionFont.render("WAITING FOR PLAYER 2", 1, (255, 0, 0))
+		connectionTextpos = connectionText.get_rect()
+		connectionTextpos.y = 140
+		self.screen.blit(connectionText, connectionTextpos)
 
 if __name__ == '__main__':
     if (len(sys.argv)!=3 and len(sys.argv)!=4) or (sys.argv[1].lower()!='runner' and sys.argv[1].lower()!='guardian'):
