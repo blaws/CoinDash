78ac5134b62b997c91b5a818f63a45af7d9c3024
amarti36 <amarti36@nd.edu>
2014-05-06 16:45:39 -0400

    changed scaling

--- Wiley.py
+++ Wiley.py
@@ -8,6 +8,7 @@ class Wiley(pygame.sprite.Sprite):
 		self.gs = gs
 		self.count = 0
 		self.image = pygame.image.load("images/Wiley1.png")
+                self.image = pygame.transform.scale(self.image, (75, 75))
 		self.rect = self.image.get_rect()
 		self.rect = self.rect.move(640, y)
 		self.yspeed = 0
@@ -19,15 +20,19 @@ class Wiley(pygame.sprite.Sprite):
 		self.animation = self.count % 8
 		if self.animation == 0:
 			self.image = pygame.image.load("images/Wiley1.png")
-                        self.image = pygame.transform.scale(self.image, (50, 50))
+                        self.image = pygame.transform.scale(self.image, (75, 75))
+			self.rect = self.image.get_rect()
 		elif self.animation == 2:
 			self.image = pygame.image.load("images/Wiley2.png")
-                        self.image = pygame.transform.scale(self.image, (50, 50))
+                        self.image = pygame.transform.scale(self.image, (75, 75))
+			self.rect = self.image.get_rect()
 		elif self.animation == 4:
 			self.image = pygame.image.load("images/Wiley3.png")
-                        self.image = pygame.transform.scale(self.image, (50, 50))
+                        self.image = pygame.transform.scale(self.image, (75, 75))
+			self.rect = self.image.get_rect()
 		elif self.animation == 6:
 			self.image = pygame.image.load("images/Wiley4.png")
-                        self.image = pygame.transform.scale(self.image, (50, 50))
+                        self.image = pygame.transform.scale(self.image, (75, 75))
+			self.rect = self.image.get_rect()
 		if self.rect.x <= -100:
 			del self.gs.wileys[self.gs.wileys.index(self)]
