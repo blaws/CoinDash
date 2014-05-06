0eeb159f17fa095f14450e77b942b8000c0f942a
amarti36 <amarti36@nd.edu>
2014-05-06 16:11:02 -0400

    cleaned guardian up

--- Guardian.py
+++ Guardian.py
@@ -8,14 +8,7 @@ from Platform import *
 from threading import Lock
 
 class Ground(pygame.sprite.Sprite):
-	def __init__(self, gs = None, x = 0, y = 0):
-#		pygame.sprite.Sprite.__init__(self)
-#		self.gs = gs
-#		if len(self.gs.grounds) != 0 and abs(self.gs.grounds[-1].rect.x - x) > 120:
-#			self.image = pygame.image.load("images/Ground2.png")
-#		else:
-#			self.image = pygame.image.load("images/Ground.png")
-#		self.rect = self.image.get_rect()
+	def __init__(self, x = 0, y = 0):
 		self.rect = Rect(x, y, 120, 120) #self.rect.move(x, y)
 		self.xspeed = -5
 		self.yspeed = 0
